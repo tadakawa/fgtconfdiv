cf32e132685efb81623915d7903442104f35c659
riceball-k <riceball@pnw.to>
2022-12-08 23:15:08 +0900

    切り出しアルゴリズム変更

--- fgt_configcut.py
+++ fgt_configcut.py
@@ -1,6 +1,20 @@
+from __future__ import annotations
+
+import collections
 import tkinter
 import tkinter.filedialog as fd
 from pathlib import Path
+
+
+class Config(collections.UserList[str]):
+    @classmethod
+    def read(cls, file: Path | str) -> Config:
+        with Path(file).open() as f:
+            return cls(f.readlines())
+
+    def write(self, file: Path | str, from_: int, to_: int) -> None:
+        with Path(file).open("wt") as f:
+            f.writelines(self[from_:to_])
 
 
 def main():
@@ -12,36 +26,39 @@ def main():
     ):
         return
 
-    for file in (Path(filename) for filename in filenames):
+    for filename in filenames:
+        file = Path(filename)
         print(f"==  {file.name}  ".ljust(79, "="))
-        with file.open() as f:
-            data = f.readlines()
+        data = Config.read(file)
 
-        indent = 0
-        start_index = 0
-        name = ""
-        for i, text in enumerate(data):
+        nested = 0
+        start_idx = 0
+        name = vdom_name = ""
+
+        for idx, text in enumerate(data):
             text = text.rstrip()
-            if text == "config vdom":
-                indent = 0
-                start_index = i
-            elif text.startswith("config"):
-                indent += 1
-                if text == "config global":
-                    start_index = i
-                    name = "global"
-            elif text.startswith("edit "):
-                indent += 1
-                if indent == 1:
-                    name = text.rstrip().replace("edit ", "")
-            elif text.startswith(("next", "end")):
-                indent -= 1
-                if indent == 0:
-                    print(f"{name=:6}: from {start_index + 1:6} to {i + 1:6}")
-                    with file.with_suffix("." + name + ".txt").open("wt") as f:
-                        f.writelines(data[slice(start_index, i+1)])
-                elif indent < 0:
-                    indent = 0
+            if text in ["config vdom", "config global"]:
+                nested = 1
+                name = vdom_name = text
+                start_idx = idx
+            elif text.startswith(("edit ", "config")):
+                nested += 1
+                if nested == 2 and text.startswith("edit "):
+                    # 2階層目の "edit xxxx" は VDOM名として一時保存
+                    vdom_name = text[5:]
+            elif text in ["next", "end"]:
+                nested -= 1
+                if nested == 1 and text == "end":
+                    # "edit VDOM名" セクションが "end" で終わったら name を更新
+                    name = vdom_name
+            elif text == "":
+                nested = 0
+                print(f"{name=:14}: from {start_idx + 1:6} to {idx + 1:6}")
+                if name:
+                    output_file = file.with_suffix("." + name + ".txt")
+                else:
+                    output_file = file.with_suffix(".txt")
+                data.write(output_file, start_idx, idx + 1)
 
 
 if __name__ == "__main__":
